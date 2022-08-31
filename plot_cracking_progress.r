dfc14b80c984d3063750d090844432ac77aa54e8
RootCubed <liam@rootcubed.dev>
2022-08-31 23:11:38 +0200

    Nicer plots

--- plot_cracking_progress.r
+++ plot_cracking_progress.r
@@ -18,11 +18,19 @@ trimmed_table <- symbols_table[
 
 options(repr.plot.width=15, repr.plot.height=8)
 
+max_val <- max(trimmed_table$count) + 500
+
 ggplot(trimmed_table, aes(x = time, y = count)) +
   ggtitle("# Symbols remaining") +
   geom_line(color = "#0c3e59", size = 1) +
   scale_x_datetime(name = "", date_labels = "%Y-%m-%d", date_breaks = "7 days") +
-  scale_y_continuous(name = "") +
+  scale_y_continuous(
+    breaks = seq(0, max_val, by = 500),
+    limits = c(0, max_val),
+    expand = c(0, 0),
+    name = ""
+  ) +
+  geom_hline(yintercept = 0, color = "black") +
   theme_fivethirtyeight() +
   theme(axis.title = element_text()) +
   theme(axis.text.x = element_text(angle = 50, hjust = 1))
